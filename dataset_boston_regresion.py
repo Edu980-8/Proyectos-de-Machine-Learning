b1b1372adc967506680fe0b48b0fc5c9b187f01b
Edu980-8 <daniedu151@gmail.com>
2024-03-10 13:01:51 -0500

    Agregando el dataset de boston a mi codigo, cabe resaltar que la version que esta en el libro Machine Learning and deep Learning de Jesus Bobadilla se encuentra deprecated, asi que se cargo de otro sitio: https://lib.stat.cmu.edu/datasets/boston , en donde se eliminan features que tenian que ver con un componente racial, y que afectaban la integridad de ciertas personas.

--- dataset_boston_regresion.py
+++ dataset_boston_regresion.py
@@ -1,1 +1,30 @@
-from sklearn.datasets import load_boston
+import pandas as pd
+import numpy as np
+
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+
+
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+
+print(data.shape)
+print(target.shape)
+
+#  CRIM     per capita crime rate by town
+#  ZN       proportion of residential land zoned for lots over 25,000 sq.ft.
+#  INDUS    proportion of non-retail business acres per town
+#  CHAS     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+#  NOX      nitric oxides concentration (parts per 10 million)
+#  RM       average number of rooms per dwelling
+#  AGE      proportion of owner-occupied units built prior to 1940
+#  DIS      weighted distances to five Boston employment centres
+#  RAD      index of accessibility to radial highways
+#  TAX      full-value property-tax rate per $10,000
+#  PTRATIO  pupil-teacher ratio by town
+#  B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+#  LSTAT    % lower status of the population
+#  MEDV     Median value of owner-occupied homes in $1000's
+
+CRIME = 0; ZN = 1; INDUS = 2; ROOMS = 5;
+
